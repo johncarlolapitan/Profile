b85e79d01d2c2ba7d3e4ea7f27ad46692ac38470
John Carlo Lapitan <110419094+johncarlolapitan@users.noreply.github.com>
2023-09-27 00:06:43 +0800

    Message

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
             </div>
             <div className="flex w-1/2 flex-col items-center self-center lg:w-full lg:text-center">
               <AnimatedText
-                text="Building Tomorrow's Digital World, One Line of Code at a Time."
+                text="Building Tomorrows Digital World, One Line of Code at a Time."
                 className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:!text-xs">
